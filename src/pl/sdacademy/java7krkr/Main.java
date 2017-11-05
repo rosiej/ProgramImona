800666278604ab370bed7c16ebcf957d1b9009ca
Rosiej <mateusz.rosiejka@gmail.com>
2017-11-05 14:34:36 +0100

    done

--- src/pl/sdacademy/java7krkr/Main.java
+++ src/pl/sdacademy/java7krkr/Main.java
@@ -1,8 +1,19 @@
 package pl.sdacademy.java7krkr;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("podaj imię");
+        Scanner scanner = new Scanner(System.in);
+       String name = scanner.nextLine();
+
+       if(name.endsWith("a")){
+           System.out.println(name +" jest kobietą");
+       }else{
+           System.out.println(name+" jest mężczyzną");
+       }
+
     }
 }
